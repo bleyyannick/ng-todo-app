3c49f23d4b41c27ecdf08cac8f205194379fa908
Yannick Bley <bleyyannickpro@gmail.com>
2024-09-02 11:14:22 +0200

    wip: styling tasks list for mobile device

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,8 +8,7 @@ import { Component } from '@angular/core';
    <header>
       <img 
         src="images/bg-desktop-dark.jpg" 
-        srcset="
-          images/bg-desktop-dark.jpg 750w,
+        srcset="images/bg-desktop-dark.jpg 750w,
           images/bg-mobile-dark.jpg 375w"
         sizes="(max-width: 375px) 375px, 
                (min-width: 376px) and (max-width: 750px) 750px, 
